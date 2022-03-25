119088b0ccceb8b80c04f41fd9b3457eccc6e305
Anghie <87211689+Anghievisck@users.noreply.github.com>
2022-03-25 08:36:10 -0300

    Add files via upload

---
+++ Ex2/src/ex2/Ex2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex2;
+
+import java.util.Scanner;
+
+public class Ex2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+       Scanner intSC = new Scanner(System.in);
+        float x = 0;
+        float soma = 0;
+        float media = -1;
+        do{
+            soma +=x;
+            media = media + 1;
+            System.out.print("Digite um número: ");
+            x = intSC.nextFloat();
+            System.out.println();
+        }while(x >= 0);
+        
+        if(media == 0){
+            System.out.println("0/0 é impossivel");
+        } else {
+            float resultado = soma/media;
+            System.out.println(soma + "/" + media + " = " + resultado);
+        }
+    }
+}
